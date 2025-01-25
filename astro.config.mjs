33d96e7de95df1aa4761fe98b5b738d5a57eb492
Bryan Valenzuela <1bryanvalenzuela@gmail.com>
2025-01-24 23:06:13 -0300

    Rem: Node, Add: Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://1bryanvalenzuela.github.io",
+  base: "portafolio",
+
   output: "static",
 
   vite: {
     plugins: [tailwindcss()],
   },
-
-  adapter: node({
-    mode: "standalone",
-  }),
 });
