604c19ff81b8e6d8900cc848ebe297546158aee9
Bryan Valenzuela <1bryanvalenzuela@gmail.com>
2025-01-31 12:49:32 -0300

    Mod: Vuelta al fondo antiguo de hero, se agrega script para optimizar el fondo, diseño de form, otros

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
-// import node from "@astrojs/node";
-// test
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
